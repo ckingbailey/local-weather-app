f4250fca086ba6da717676c2d9ef880e03384e72
ckingbailey <ckingbailey@gmail.com>
2017-04-06 20:46:14 -0700

    continuing to build script file for new fog animation. ohmigod I need to stop. it's time to wind down and go to bed.

--- animations/fog/codepen/fog.js
+++ animations/fog/codepen/fog.js
@@ -1,6 +1,6 @@
 (function fog(){
-var appendMe;
-var appendHere;
+var appendMe = document.createElement('div');
+var appendHere = document.querySelector('.sky');
 var layers = [sub, background, midground, foreground, sup];
 
 for(i=0; i<layers.length; i++){
