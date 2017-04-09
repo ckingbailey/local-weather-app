e5bd8d37f711366df70d68186cd7fddcf28651d1
ckingbailey <ckingbailey@gmail.com>
2017-04-09 14:08:14 -0700

    clean up file structure

--- animations/fog/codepen/fog.js
+++
@@ -1,9 +0,0 @@
-(function fog(){
-var appendMe = document.createElement('div');
-var appendHere = document.querySelector('.sky');
-var layers = [sub, background, midground, foreground, sup];
-
-for(i=0; i<layers.length; i++){
-
-}
-})();
