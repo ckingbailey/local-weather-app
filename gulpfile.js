03df28338851e9f610271102e863e0b7bcc35ea0
ckingbailey <ckingbailey@gmail.com>
2017-04-15 14:15:33 -0700

    noticed gulp-webpack runs webpack 1.14, but the modules syntax I know is ES modules, which is supported only by webpack 2. After struggling with this, I'm committing now before I change over to use npm scripts and webpack without gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const webpack = require('gulp-webpack');
+const rename = require('gulp-rename');
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass-min', function() {
+  return gulp.src('./scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('style-min.css'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('bundle', function() {
+  return gulp.src('./js/index.js')
+    .pipe(webpack(
+      {
+        output: {
+          filename: 'weather.js'
+        }
+      }
+    ))
+    .pipe(gulp.dest('./js'));
+});
