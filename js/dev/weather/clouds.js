f35207a9bdff08710b7d84f3a40f9839b4e69671
ckb <ckingbailey@gmail.com>
2017-05-09 22:25:20 +0000

    del console.log(w) from clouds.js

--- js/dev/weather/clouds.js
+++ js/dev/weather/clouds.js
@@ -14,7 +14,7 @@ export default function clouds(){
 
     appendMe.addEventListener('animationiteration', function() {
       var w = window.getComputedStyle(this).width.match(/(\d+)(\D+)/);
-      console.log(w);
+      
       this.style.left = -(parseInt(w[1], 10) + (parseInt(w[1], 10) * 0.05)) + w[2];
     }, {once: true});
 
